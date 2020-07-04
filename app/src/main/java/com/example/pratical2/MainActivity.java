5c5e5b3b543517c92c5d8af3a557c41fabf57374
idontknow-howtocode <66990505+idontknow-howtocode@users.noreply.github.com>
2020-07-04 16:11:13 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/pratical2/MainActivity.java
+++ app/src/main/java/com/example/pratical2/MainActivity.java
@@ -1,22 +1,28 @@
-package com.example.pratical2;
+package com.example.practical1_2b;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int mCount = 0;
+    private int mCount = 0, mReset = 0;
     private TextView mShowCount;
+    private Button mResetButton;
+    private Button mCountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        mResetButton = findViewById(R.id.button_zero);
+        mCountButton = findViewById(R.id.button_count);
     }
 
     public void showToast(View view) {
@@ -27,5 +33,24 @@ public class MainActivity extends AppCompatActivity {
     public void countUp(View view) {
         mCount++;
         if (mShowCount != null) mShowCount.setText(Integer.toString(mCount));
+        mResetButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        mResetButton.setClickable(true);
+
+       switch (mCount % 2) {
+            case 0: view.setBackgroundColor(Color.MAGENTA);
+                break;
+            case 1: view.setBackgroundColor(Color.GREEN);
+                break;
+            default: break;
+        }
+    }
+
+    public void resetZero(View view) {
+        mReset = 0;
+        mShowCount.setText(String.valueOf(mReset));
+
+        view.setClickable(false);
+        view.setBackgroundColor(Color.GRAY);
+        mCountButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
     }
 }
